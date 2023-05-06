1dcf02068ecab44bd1979284b49d6e38f1332a3b
Joshua Cowell <joshuacowell@hotmail.com>
2023-05-06 00:19:39 +0000

    add database folder

---
+++ src/db/database.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import { MessageData } from "../classes/MessageData";
+
+export const prisma: PrismaClient = new PrismaClient();
+
+async function createUser(data: MessageData) {
+
+    const user = prisma.user.create({data: {
+        id: parseInt(data.userId),
+        name: data.username
+    }});
+
+    return user;
+}
+
+async function createVideo(data: MessageData) {
+    const video = await prisma.videos.create({
+        data: {
+            link: "test",
+            duration: "10:00",
+            userId: parseInt(data.userId)
+        }
+    })
+    return video;
+}
+
+export async function createVideoEntry(data: MessageData) {
+    const userCount = await prisma.user.count({
+        where: {
+            id: parseInt(data.userId)
+        }
+    })
+    if (userCount == 0) createUser(data);
+    return await createVideo(data);
+}
